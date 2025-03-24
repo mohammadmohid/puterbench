82c77b1331f4159a7b23cd34ca5f322c5d34cfbe
Mohammad Mohid <49339308+mohammadmohid@users.noreply.github.com>
2025-03-25 01:15:21 +0500

    Fix Vercel deployment - Next.js & Express

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "https://puterbench.vercel.app/api/:path*",
+      },
+    ];
+  },
   images: {
     domains: ["res.cloudinary.com"],
   },
