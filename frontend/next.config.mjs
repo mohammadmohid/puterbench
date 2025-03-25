0bdbc78c8678ea72bc1e44b6cc8d8080d0fc1bd1
Mohammad Mohid <49339308+mohammadmohid@users.noreply.github.com>
2025-03-25 23:49:37 +0500

    Type fix in next config

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    removePatterns: [
+    remotePatterns: [
       {
         protocol: "https",
         hostname: "res.cloudinary.com",
